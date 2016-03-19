9f3bff3e3474e3dfcb8de002a03d1e18048aab5e
Selvio Perez <selvio89@hotmail.com>
2016-03-19 09:25:30 -0500

    First commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+
+//= require jquery/dist/jquery
+//= require angular/angular
+
+//= require ./angular/angular
